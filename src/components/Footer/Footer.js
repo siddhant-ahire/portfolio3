b762200eca4251b4e2ab57acafff3f8708b936a6
siddhantahireofficial <siddhantahireofficial@gmail.com>
2021-05-20 22:15:59 +0530

    footer added '

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,18 @@
+import { Typography } from '@material-ui/core'
 import React from 'react'
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            Footer
+        <div className="footer">
+            <div className="footer_left">
+                <Typography >siddhant ahire</Typography>
+            </div>
+            <div className="footer_right">
+                <Typography className="copyright_text">
+                    Designed and Developed by <a href="/" target="_blank">Siddhant Ahire</a>
+                </Typography>
+            </div>
         </div>
     )
 }
